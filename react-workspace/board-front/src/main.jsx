2c2c99881984aafb48205e31521e298c5ee189ec
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-07-02 16:56:08 +0900

    678

--- react-workspace/board-front/src/main.jsx
+++ react-workspace/board-front/src/main.jsx
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Header />
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Join" element={<Join />} />
       <Route path="/BoardList" element={<BoardList />} />
